2b74f3907beee718f5ede2f90b3d83bd0ac6342e
jayzzer <jayzzer@users.noreply.github.com>
2022-03-14 17:46:39 +0000

    SHOOT-0: apply linters fixes

--- Assets/Plugins/PersistentSceneLoader/Scripts/Editor/SceneMenuList.cs
+++ Assets/Plugins/PersistentSceneLoader/Scripts/Editor/SceneMenuList.cs
@@ -1,20 +1,23 @@
-﻿// This class is Auto-Generated. Don't delete it!
+// This class is Auto-Generated. Don't delete it!
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
-public static class SceneMenuList {
+public static class SceneMenuList
+{
 
-    [MenuItem("Scenes/PersistentScene")]
-    private static void OpenPersistentScene() {
-        EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Single);
-        EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Additive);
-    }
+	[MenuItem("Scenes/PersistentScene")]
+	private static void OpenPersistentScene()
+	{
+		EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Single);
+		EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Additive);
+	}
 
-    [MenuItem("Scenes/TestScene")]
-    private static void OpenTestScene() {
-        EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Single);
-        EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/TestScene/TestScene.unity", OpenSceneMode.Additive);
-    }
+	[MenuItem("Scenes/TestScene")]
+	private static void OpenTestScene()
+	{
+		EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/PersistentScene/PersistentScene.unity", OpenSceneMode.Single);
+		EditorSceneManager.OpenScene("Assets/ShooterProject/Scenes/TestScene/TestScene.unity", OpenSceneMode.Additive);
+	}
 
 
 }
