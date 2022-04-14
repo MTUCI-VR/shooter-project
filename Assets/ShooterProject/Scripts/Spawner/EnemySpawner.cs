56b95a297782e2983db858e37ea0993a07e210a0
Arenchik <93075694+Arenchick@users.noreply.github.com>
2022-04-14 09:57:11 +0300

    SHOOT-37: Spawner (#19)
    * SHOOT-37: Add General Spawner system
    
    * SHOOT-37: End
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-37: Redesigned spawning system
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-37: Finish
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-37: FInish
    
    * SHOOT-0: apply linters fixes
    
    Co-authored-by: Arenchick <Arenchick@users.noreply.github.com>

---
+++ Assets/ShooterProject/Scripts/Spawner/EnemySpawner.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using ShooterProject.Scripts.PlayerScripts;
+
+namespace ShooterProject.Scripts.Spawner
+{
+	public class EnemySpawner : GeneralSpawner
+	{
+		#region Fields
+
+		[SerializeField]
+		private int spawnDelayInSeconds;
+
+		[SerializeField]
+		private string playerTag;
+
+		private bool _canSpawn = true;
+
+		#endregion
+
+		#region  LifeCycle
+
+		private void Start()
+		{
+			StartCoroutine(EnemySpawn());
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private IEnumerator EnemySpawn()
+		{
+			while (true)
+			{
+				yield return new WaitForSeconds(spawnDelayInSeconds);
+
+				if (!_canSpawn) yield break;
+
+				Spawn();
+			}
+		}
+
+		private void OnTriggerEnter(Collider collider)
+		{
+			if (collider.TryGetComponent<Player>(out Player player))
+			{
+				_canSpawn = false;
+			}
+		}
+		private void OnTriggerExit(Collider collider)
+		{
+			if (collider.TryGetComponent<Player>(out Player player))
+			{
+				StartCoroutine(EnemySpawn());
+
+				_canSpawn = true;
+			}
+		}
+
+		#endregion
+	}
+}
