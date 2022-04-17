1664594b00196872db601ecf899a48ef74222fde
Arenchick <Arenchick@users.noreply.github.com>
2022-04-17 16:51:15 +0000

    SHOOT-0: apply linters fixes

--- Assets/ShooterProject/Scripts/InteractableThings/ObjectHighlight.cs
+++ Assets/ShooterProject/Scripts/InteractableThings/ObjectHighlight.cs
@@ -4,78 +4,78 @@ using ShooterProject.Scripts.Items;
 
 namespace ShooterProject.Scripts.InteractableThings
 {
-    public class ObjectHighlight : MonoBehaviour
-    {
-        #region Fields
+	public class ObjectHighlight : MonoBehaviour
+	{
+		#region Fields
 
-        [SerializeField]
-        private XRDirectInteractor directInteractor;
+		[SerializeField]
+		private XRDirectInteractor directInteractor;
 
-        [SerializeField] 
-        private LayerMask objectHighlightLayer;
+		[SerializeField]
+		private LayerMask objectHighlightLayer;
 
-        private int _objectHighlightLayerValue;
+		private int _objectHighlightLayerValue;
 
-        #endregion
+		#endregion
 
-        #region Life Cycle
+		#region Life Cycle
 
-        private void Awake()
-        {
-            _objectHighlightLayerValue = (int)System.Math.Log(objectHighlightLayer,2);
-        }
+		private void Awake()
+		{
+			_objectHighlightLayerValue = (int)System.Math.Log(objectHighlightLayer, 2);
+		}
 
-        private void OnEnable()
-        {
-            directInteractor.selectEntered.AddListener(OnObjectHighlightDisable);
-            directInteractor.selectExited.AddListener(OnObjectHighlightEnable);
-        }
-        private void OnDisable()
-        {
-            directInteractor.selectEntered.RemoveListener(OnObjectHighlightDisable);
-            directInteractor.selectExited.RemoveListener(OnObjectHighlightEnable);
-        }
+		private void OnEnable()
+		{
+			directInteractor.selectEntered.AddListener(OnObjectHighlightDisable);
+			directInteractor.selectExited.AddListener(OnObjectHighlightEnable);
+		}
+		private void OnDisable()
+		{
+			directInteractor.selectEntered.RemoveListener(OnObjectHighlightDisable);
+			directInteractor.selectExited.RemoveListener(OnObjectHighlightEnable);
+		}
 
-        #endregion
+		#endregion
 
-        #region Private Methods
+		#region Private Methods
 
-        private void OnTriggerEnter(Collider collider)
-        {
-            if(collider.TryGetComponent<Item>(out Item item) && !collider.gameObject.GetComponent<XRGrabInteractable>().isSelected) 
-            {
-                ObjectHighlightEnable(collider.gameObject);                
-            }
-        }
+		private void OnTriggerEnter(Collider collider)
+		{
+			if (collider.TryGetComponent<Item>(out Item item) && !collider.gameObject.GetComponent<XRGrabInteractable>().isSelected)
+			{
+				ObjectHighlightEnable(collider.gameObject);
+			}
+		}
 
-        private void OnTriggerExit(Collider collider)
-        {
-            if(collider.TryGetComponent<Item>(out Item item))
-            {
-                ObjectHighlightDisable(collider.gameObject);
-            }
-        }
+		private void OnTriggerExit(Collider collider)
+		{
+			if (collider.TryGetComponent<Item>(out Item item))
+			{
+				ObjectHighlightDisable(collider.gameObject);
+			}
+		}
 
-        private void OnObjectHighlightDisable(SelectEnterEventArgs selectEnterEventArgs)
-        {
-            ObjectHighlightDisable(selectEnterEventArgs.interactableObject.transform.gameObject);
-        }
-        
-        private void OnObjectHighlightEnable(SelectExitEventArgs selectExitEventArgs)
-        {
-            ObjectHighlightEnable(selectExitEventArgs.interactableObject.transform.gameObject);
-        }
+		private void OnObjectHighlightDisable(SelectEnterEventArgs selectEnterEventArgs)
+		{
+			ObjectHighlightDisable(selectEnterEventArgs.interactableObject.transform.gameObject);
+		}
 
-        private void ObjectHighlightEnable(GameObject objectHighlight)
-        {
-            objectHighlight.layer = _objectHighlightLayerValue;
-        }
+		private void OnObjectHighlightEnable(SelectExitEventArgs selectExitEventArgs)
+		{
+			ObjectHighlightEnable(selectExitEventArgs.interactableObject.transform.gameObject);
+		}
 
-        private void ObjectHighlightDisable(GameObject objectHighlight)
-        {
-            objectHighlight.layer = objectHighlight.GetComponent<Item>().initialLayerValue;
-        }
+		private void ObjectHighlightEnable(GameObject objectHighlight)
+		{
+			objectHighlight.layer = _objectHighlightLayerValue;
+		}
 
-        #endregion
-    }
+		private void ObjectHighlightDisable(GameObject objectHighlight)
+		{
+			objectHighlight.layer = objectHighlight.GetComponent<Item>().initialLayerValue;
+		}
+
+		#endregion
+	}
 }
