55b5253439576abdfed5f0d59190b1ee83e2761d
EgorKirnosov <kirnosov.eg@yandex.ru>
2022-04-17 16:26:09 +0300

    SHOOT-10: Moved LayerUtils and deleted knife script

--- Assets/ShooterProject/Scripts/Actors/Health/DamageTriggerZone.cs
+++ Assets/ShooterProject/Scripts/Actors/Health/DamageTriggerZone.cs
@@ -70,13 +70,4 @@ namespace ShooterProject.Scripts.Actors.Health
 		#endregion
 	}
 }
-namespace ShooterProject.Scripts.General
-{
-	public static class LayerUtils
-	{
-		public static bool IsLayerMatch(LayerMask sourceLayer, int testingLayer)
-		{
-			return (sourceLayer & (1 << testingLayer)) != 0;
-		}
-	}
-}
+
---
+++ Assets/ShooterProject/Scripts/General/LayerUtils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+namespace ShooterProject.Scripts.General
+{
+	public static class LayerUtils
+	{
+		public static bool IsLayerMatch(LayerMask sourceLayer, int testingLayer)
+		{
+			return (sourceLayer & (1 << testingLayer)) != 0;
+		}
+	}
+}
