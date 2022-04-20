ad31148a47fffb8ec84a313b60c41123eec2bba4
Arenchick <Arenchick@users.noreply.github.com>
2022-04-20 12:32:44 +0000

    SHOOT-0: apply linters fixes

--- Assets/ShooterProject/Scripts/Items/FirstAidKit/FirstAidKit.cs
+++ Assets/ShooterProject/Scripts/Items/FirstAidKit/FirstAidKit.cs
@@ -4,51 +4,51 @@ using ShooterProject.Scripts.Actors.Health;
 
 namespace ShooterProject.Scripts.Items.FirstAidKit
 {
-    [RequireComponent(typeof(XRGrabInteractable))]
-    public class FirstAidKit : MonoBehaviour
-    {   
-        #region Fields
+	[RequireComponent(typeof(XRGrabInteractable))]
+	public class FirstAidKit : MonoBehaviour
+	{
+		#region Fields
 
-        [SerializeField]
-        private int healthPointsForHealing;
+		[SerializeField]
+		private int healthPointsForHealing;
 
-        [SerializeField]
-        private Health playerHealth;
+		[SerializeField]
+		private Health playerHealth;
 
-        private XRGrabInteractable _grabInteractable;
+		private XRGrabInteractable _grabInteractable;
 
-        #endregion
+		#endregion
 
-        #region Life Cycle
+		#region Life Cycle
 
-        private void Awake()
-        {
-            _grabInteractable = GetComponent<XRGrabInteractable>();
-        }
+		private void Awake()
+		{
+			_grabInteractable = GetComponent<XRGrabInteractable>();
+		}
 
-        private void OnEnable()
-        {
-            _grabInteractable.activated.AddListener(OnActivated);
-        }
+		private void OnEnable()
+		{
+			_grabInteractable.activated.AddListener(OnActivated);
+		}
 
-        private void OnDisable()
-        {
-            _grabInteractable.activated.RemoveListener(OnActivated);
-        }
+		private void OnDisable()
+		{
+			_grabInteractable.activated.RemoveListener(OnActivated);
+		}
 
-        #endregion
+		#endregion
 
-        #region Private Methods
+		#region Private Methods
 
-        private void OnActivated(ActivateEventArgs activateEventArgs)
-        {
-            if (playerHealth.CurrentHealth < playerHealth.MaxHealth)
-            {
-                playerHealth.Heal(healthPointsForHealing);
-                Destroy(gameObject);
-            }
-        }
+		private void OnActivated(ActivateEventArgs activateEventArgs)
+		{
+			if (playerHealth.CurrentHealth < playerHealth.MaxHealth)
+			{
+				playerHealth.Heal(healthPointsForHealing);
+				Destroy(gameObject);
+			}
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
