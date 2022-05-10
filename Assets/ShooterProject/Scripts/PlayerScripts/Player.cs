25bbcb1063c0928c2845ccb6d3d20718e0e9d20e
Arenchik <93075694+Arenchick@users.noreply.github.com>
2022-05-10 21:37:24 +0300

    SHOOT-42: First Aid Kit (#22)
    * SHOOT-42: Create First aid kit
    
    * SHOOT-42: Add use condition
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-42: Fix remarks
    
    * SHOOT-0: apply linters fixes
    
    Co-authored-by: Arenchick <Arenchick@users.noreply.github.com>

--- Assets/ShooterProject/Scripts/PlayerScripts/Player.cs
+++ Assets/ShooterProject/Scripts/PlayerScripts/Player.cs
@@ -1,9 +1,35 @@
 using UnityEngine;
+using ShooterProject.Scripts.Actors.Health;
 
 namespace ShooterProject.Scripts.PlayerScripts
 {
+	[RequireComponent(typeof(Health))]
 	public class Player : MonoBehaviour
 	{
+		#region Fields
 
+		public static Player instance;
+
+		public Health PlayerHealth { get; private set; }
+
+		#endregion
+
+		#region Life Cycle
+
+		private void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+
+			PlayerHealth = GetComponent<Health>();
+		}
+
+		#endregion
 	}
 }
