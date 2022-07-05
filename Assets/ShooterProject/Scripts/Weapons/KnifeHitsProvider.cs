5b6ff095a8bf0bbd793f036afef8ba0180ec3367
MusaFairy <36456084+nadvista@users.noreply.github.com>
2022-07-05 19:35:24 +0300

    SHOOT-14: Knife (#26)
    * SHOOT-14: Added the knife testing scene, knife prefab
    
    * SHOOT-14: Edited knife prefab
    
    * SHOOT-14: did damage depending on the speed of the knife
    
    * SHOOT-14: Set up the scene and prefab
    
    * SHOOT-14: Finished the knife
    
    * SHOOT-14: Added regions
    
    * SHOOT-14: Fixed the bugs
    
    * SHOOT-0: apply linters fixes
    
    Co-authored-by: Пушкарев Павел Сергеевич <ppushkarev@phoenixit.ru>
    Co-authored-by: jayzzer <jayzzer@users.noreply.github.com>

---
+++ Assets/ShooterProject/Scripts/Weapons/KnifeHitsProvider.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ShooterProject.Scripts.General;
+using ShooterProject.Scripts.Actors.Health;
+using UnityEngine.XR.Interaction.Toolkit;
+namespace ShooterProject.Scripts.Weapons
+{
+	[RequireComponent(typeof(Collider), typeof(XRBaseInteractable))]
+	public class KnifeHitsProvider : MonoBehaviour
+	{
+
+		#region Fields
+
+		[SerializeField]
+		private LayerMask interactionLayer;
+
+		[SerializeField]
+		[Min(0)]
+		private float damage;
+
+		[SerializeField]
+		private float hitCoolDownSeconds;
+
+		[SerializeField]
+		private float minHitVelocity;
+
+		[SerializeField]
+		private float minHitAngularVelocity;
+
+		private XRBaseInteractable _xrInteractable;
+		private Health _target;
+		private bool _coolDownOver;
+		private Vector3 _previousPosition;
+		private Quaternion _previousRotation;
+
+		#endregion
+
+		#region Properties
+
+		public bool CanHit => _xrInteractable.isSelected && _coolDownOver && _target != null;
+
+		#endregion
+
+		#region LifeCycle
+
+		private void Awake()
+		{
+			_xrInteractable = GetComponent<XRBaseInteractable>();
+		}
+
+		private void Start()
+		{
+			SetupPositionRotation();
+		}
+
+		private void OnEnable()
+		{
+			_coolDownOver = true;
+		}
+		private void OnDisable()
+		{
+			ResetTarget();
+		}
+		private void Update()
+		{
+			if (CanHit)
+			{
+				var velocity = GetVelocity();
+				var angularVelocity = GetAngularVelocity();
+
+				SetupPositionRotation();
+				TryHit(velocity, angularVelocity);
+			}
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.TryGetComponent<Health>(out var targetHealth)
+				&& targetHealth.CurrentHealth > 0
+				&& LayerUtils.IsLayerMatch(interactionLayer, other.gameObject.layer))
+			{
+				_target = targetHealth;
+				_target.OnDied += ResetTarget;
+			}
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (other.TryGetComponent<Health>(out var otherHealth)
+			&& _target == otherHealth)
+			{
+				ResetTarget();
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private float GetAngularVelocity()
+		{
+			var rotationDelta = transform.rotation * Quaternion.Inverse(_previousRotation);
+			var angularVelocity = (rotationDelta.eulerAngles / Time.deltaTime).magnitude;
+			return angularVelocity;
+		}
+
+		private float GetVelocity()
+		{
+			var positionDelta = transform.position - _previousPosition;
+			var velocity = (positionDelta / Time.deltaTime).magnitude;
+			return velocity;
+		}
+
+		private void SetupPositionRotation()
+		{
+			_previousPosition = transform.position;
+			_previousRotation = transform.rotation;
+		}
+
+		private void ResetTarget()
+		{
+			_target.OnDied -= ResetTarget;
+			_target = null;
+		}
+
+		private void TryHit(float currentVelocity, float angularVelocity)
+		{
+			if (currentVelocity >= minHitVelocity || angularVelocity >= minHitAngularVelocity)
+			{
+				_target.TakeHit(damage);
+				StartCoroutine(HitCoolDownCoroutine());
+			}
+		}
+
+		private IEnumerator HitCoolDownCoroutine()
+		{
+			_coolDownOver = false;
+			yield return new WaitForSeconds(hitCoolDownSeconds);
+			_coolDownOver = true;
+		}
+
+		#endregion
+
+	}
+
+}
+
