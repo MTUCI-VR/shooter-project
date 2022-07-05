cd15b8e5d3a1c24e3b15f33c2ea8ac701e04a9aa
Arenchik <93075694+Arenchick@users.noreply.github.com>
2022-07-05 19:39:41 +0300

    SHOOT-40: MainMenu (#29)
    * SHOOT-40: Create scene
    
    * SHOOT-40: Create Scene Loader script
    
    * SHOOT-40: Finish
    
    * SHOOT-40: Upgrading scripts
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-40: Redisigned menu scripts
    
    * SHOOT-40: End
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-40: Upgrade scripts
    
    * SHOOT-0: apply linters fixes
    
    * SHOOT-40: Fix remarks
    
    Co-authored-by: Arenchick <Arenchick@users.noreply.github.com>

---
+++ Assets/ShooterProject/Scripts/GameManager/SceneLoader.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ShooterProject.Scripts.GameManager
+{
+	public static class SceneLoader
+	{
+		#region Fields
+
+		private static float _progress;
+
+		#endregion
+
+		#region Properties
+
+		public static float Progress
+		{
+			get => _progress;
+
+			private set
+			{
+				var tolerance = 0.000000001;
+
+				if (!(Math.Abs(_progress - value) < tolerance))
+				{
+					_progress = value;
+					OnProgressChanged?.Invoke();
+				}
+			}
+		}
+
+		#endregion
+
+		#region Events
+
+		public static event Action OnProgressChanged;
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Загружает указанную сцену
+		/// </summary>
+		/// <param name="sceneName">Название сцены для перехода</param>
+		public static IEnumerator LoadScene(string sceneName)
+		{
+			AsyncOperation sceneAsyncOperation = SceneManager.LoadSceneAsync(sceneName);
+
+			Progress = 0;
+
+			do
+			{
+				Progress = sceneAsyncOperation.progress * (10f / 9f);
+
+				yield return new WaitForEndOfFrame();
+
+			} while (!sceneAsyncOperation.isDone);
+		}
+
+		#endregion
+	}
+}
